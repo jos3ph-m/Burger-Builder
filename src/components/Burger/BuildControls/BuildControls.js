dbc2443c4cb1b6f9e0476e3206407d71194636b9
Joseph M <jmedina1985@hotmail.com>
2020-07-28 15:13:28 -0700

    Removing ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -19,6 +19,8 @@ const buildControls = (props) => {
           key={ctrl.label}
           label={ctrl.label}
           added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
         />
       ))}
     </div>
