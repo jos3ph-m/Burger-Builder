c2a45e075b528c87cfc612de6112be148abc303c
Joseph M <jmedina1985@hotmail.com>
2020-07-30 15:19:08 -0700

    Purchase handler

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -26,7 +26,11 @@ const buildControls = (props) => {
           disabled={props.disabled[ctrl.type]}
         />
       ))}
-      <button className={classes.OrderButton} disabled={!props.purchasable}>
+      <button
+        className={classes.OrderButton}
+        disabled={!props.purchasable}
+        onClick={props.ordered}
+      >
         Place Order
       </button>
     </div>
